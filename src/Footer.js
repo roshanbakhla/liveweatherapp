d297346589cd0236a3c1e93d9955eb7d67e2a5d4
Roshan Bakhla <roshanbakhla@Roshans-MacBook-Air.local>
2022-07-09 12:05:39 +0530

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="footer-box">
+        <p> © 2022 Roshan Bakhla. All rights reserved</p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
